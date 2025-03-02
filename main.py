6a6d57545cfbd9915bac5f20574ae1746b26bb94
Milos Cvetkovic <milosnet22@gmail.com>
2025-03-02 14:51:09 +0100

    Added Cart functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Kreiranje main.py datoteke
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 if __name__ == "__main__":
     manager = ProductManager()
@@ -14,3 +15,12 @@ if __name__ == "__main__":
 
     manager.display_products()
     print("Total Inventory Value:", manager.total_inventory_value())
+    
+    if __name__ == "__main__":
+       cart = Cart()
+       cart.add_to_cart(product1)
+       cart.add_to_cart(product2)
+       cart.add_to_cart(product3)
+
+       cart.display_cart()
+       print("Total Cart Value:", cart.total_cart_value())
