e49069c7557cddc53a9f8f1ba5d511615c93ced1
Milos Cvetkovic <milosnet22@gmail.com>
2025-03-02 14:28:54 +0100

    Initial commit-Product class implemented

---
+++ product.py
@@ -0,0 +1,12 @@
+# Kreiranje klase product
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def display_info(self):
+        return f"{self.name}: ${self.price}, Quantity: {self.quantity}"
+
+    def update_quantity(self, new_quantity):
+        self.quantity = new_quantity
